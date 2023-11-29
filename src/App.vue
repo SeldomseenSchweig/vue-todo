ea0c5443c229af223c5699c038b0b7432a6bf352
SeldomseenSchweig <jamesschweig@gmail.com>
2023-11-29 12:48:29 -0800

    working on vue todo app

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+const addTodo = () => {};
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's Up? <input type="text" Placeholder="name here" v-model="name" />
+      </h2>
+    </section>
+    <section class="create-todo">
+      <h3>CREATE TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What is on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. go shopping "
+          v-model="input_content"
+        />
+        <h4>Pick a Category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
